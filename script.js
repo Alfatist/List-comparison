1b89bf7d83a8f6e4fc15f13ba4e93bf652aaf749
Mateus Henrique <mateushprofissional@gmail.com>
2023-06-23 16:08:08 -0300

    V1.1.0

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const fs = require("fs");
 const output = "./output/";
+function comparar() {
+  let setores = fs.readFileSync("./lista.txt", "utf8");
 
-let setores = fs.readFileSync("./lista.txt", "utf8");
+  let comparacao = fs.readFileSync("./comparacao.txt", "utf8");
 
-let comparacao = fs.readFileSync("./comparacao.txt", "utf8");
+  let array = setores.split("\n");
+  let array_comparacao = comparacao.split("\n");
+  let novo_texto = [];
 
-let array = setores.split("\n");
-let array_comparacao = comparacao.split("\n");
-let novo_texto = [];
-
-for (let i = 0; i < array.length; ++i) {
-  if (array_comparacao.indexOf(array[i]) === -1) {
-    novo_texto.push(array[i]);
+  for (let i = 0; i < array.length; ++i) {
+    if (array_comparacao.indexOf(array[i]) === -1) {
+      novo_texto.push(array[i]);
+    }
   }
+
+  resultado = novo_texto.join("\n");
+
+  if (!fs.existsSync(output)) {
+    fs.mkdirSync(output);
+  }
+  fs.writeFileSync(output + "resultado.txt", resultado);
 }
 
-resultado = novo_texto.join("\n");
-
-if (!fs.existsSync(output)) {
-  fs.mkdirSync(output);
-}
-fs.writeFileSync(output + "resultado.txt", resultado);
+comparar();

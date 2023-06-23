c651ff889f700cb994d1341f31a37674e0621a72
Mateus Henrique <mateushprofissional@gmail.com>
2023-06-23 15:30:08 -0300

    V1.0.0 - Comparação feita com nodeJS

---
+++ script.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const output = "./output/";
+
+let setores = fs.readFileSync("./lista.txt", "utf8");
+
+let comparacao = fs.readFileSync("./comparacao.txt", "utf8");
+
+let array = setores.split("\n");
+let array_comparacao = comparacao.split("\n");
+let novo_texto = [];
+
+for (let i = 0; i < array.length; ++i) {
+  if (array_comparacao.indexOf(array[i]) === -1) {
+    novo_texto.push(array[i]);
+  }
+}
+
+resultado = novo_texto.join("\n");
+
+if (!fs.existsSync(output)) {
+  fs.mkdirSync(output);
+}
+fs.writeFileSync(output + "resultado.txt", resultado);
